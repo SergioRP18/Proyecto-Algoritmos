c8016319d60f89d3575742092247480e7c9367fc
SergioRP18 <sergio.restrepo@u.icesi.edu.co>
2024-09-25 15:19:18 -0500

    Refactor: termina la estructura de la navBar pero responsive

--- Trip Share/src/components/indexPadre.ts
+++ Trip Share/src/components/indexPadre.ts
@@ -3,3 +3,4 @@ export { default as navAside } from './AsideHome/navAside';
 export { default as UserBar } from './UserBar/userBar';
 export { default as AppPost } from './cardPost/post';
 export { default as SearchBar } from './SearchBar/searchBar';
+export { default as NavResponsive } from './navBar/navBarResponsive/NavResponsive';
