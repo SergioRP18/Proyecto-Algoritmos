72169681fcec183afa72430e186495337c7cfed8
SergioRP18 <sergio.restrepo@u.icesi.edu.co>
2024-10-27 15:24:38 -0500

    Termina el componente del input que permite agregar texto al post

--- Trip Share/src/components/CreatePost/inputPost.ts
+++ Trip Share/src/components/CreatePost/inputPost.ts
@@ -8,7 +8,17 @@ class Post extends HTMLElement {
     }
 
     render(){
-
+        if(this.shadowRoot){
+            this.shadowRoot.innerHTML = `
+                <h1>Write your review</h1>
+                <input type="text" id="description" placeholder="Every photo has a story..." required>
+                <h1>Your Hashtags</h1>
+                <input type="text" id="labels" placeholder="How do you identify this moment..." required>
+                <h1>Your Location</h1>
+                <input type="text" id="location" placeholder="Help others find this place..." required>
+                <button class="update">Publish</button>
+            `;
+        }
     }
 };
 customElements.define("section-post", Post);
