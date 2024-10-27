fb17012ae635c99db41a0d4d6108aa178ef4af72
SergioRP18 <sergio.restrepo@u.icesi.edu.co>
2024-10-26 21:42:08 -0500

    feat: Crea la class para el login en las screens

---
+++ Trip Share/src/screens/Login/login.ts
@@ -0,0 +1,15 @@
+class AppLogin extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode:'open'});
+    }
+
+    async connectedCallback(){
+
+    }
+
+    return(){
+        
+    }
+};
+customElements.define("app-login", AppLogin)
