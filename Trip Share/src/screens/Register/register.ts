b435d307815372b0d89472a1c836a1ec6f38a304
SergioRP18 <sergio.restrepo@u.icesi.edu.co>
2024-10-26 21:39:57 -0500

    feat: Crea la class para el registro en las screens

---
+++ Trip Share/src/screens/Register/register.ts
@@ -0,0 +1,15 @@
+class AppRegister extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode:'open'});
+    }
+
+    async connectedCallback(){
+
+    }
+
+    render(){
+        
+    }
+};
+customElements.define("app-register", AppRegister)
